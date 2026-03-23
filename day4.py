373d5f403e8d759d3495f80595daf9ff76f14af0
Theekshani1103 <theekshani1104@gmail.com>
2026-03-23 13:11:26 +0530

    conditional statements

---
+++ day4.py
@@ -0,0 +1,79 @@
+'''
+if statement --> if statement is used to cgeck any condition,if the condition becomes true then it will enter into inside the (if statement)'''
+
+'''
+age = int(input("Enter your age: "))
+if age >= 18:
+    print("your age is 18 or above")'''
+
+'''student_att = int (input("pls enter your sem attendence:"))
+if student_att >= 75:
+    print("you can directly sit in the sem exam")'''
+
+
+'''if -else statement --> this is also called as fallback statement
+which will only execute when the(if statement)becomes false 
+'''
+'''age = int(input("Enter your age: "))
+if age >= 18:
+    print("you can vote")
+else:
+    print("you cannot vote and you have to wait{18-age} years")'''
+
+'''total_am = int(input("Enter the total shopping money:"))
+if total_am >= 149:
+    print("no delivery charges")
+else:
+    print(f"add {149-total_am } to your cart")
+
+'''
+
+'''
+total_marks = int(input("Enter marks:"))
+if total_marks >= 65:                 
+    print("passed")
+else:
+    print("not passed")'''
+
+
+
+'''if -elif-else statement --> in the  elif (if+else) part ,i can check one more condition '''
+'''student_marks = int(input("Enter your marks:"))
+if student_marks >= 90:
+    print("you got A+ grade")
+elif student_marks >= 75 and student_marks < 90:
+    print("you got A grade")
+elif student_marks >= 60 and student_marks < 75:
+    print("you got B+ grade")
+else:
+    print("you failed")'''
+
+'''
+num_1 = int(input("Enter 1st number:"))
+num_2 = int(input("Enter 2nd number:"))
+calculate_numbers = int(input("Enter a number:"))
+if calculate_numbers == "+":
+    print( num_1 + num_2)
+elif calculate_numbers == "-":
+    print (num_1 - num_2)
+elif calculate_numbers == "*":
+    print (num_1 * num_2)
+else :
+    print (num_1/num_2)'''
+
+'''num_1 = int(input("Enter 1st number:"))
+num_2 = int(input("Enter 2nd number:"))
+users_choice = int(input("Enter your choice \n1.Add \n2.Sub\n3.Mul\n4.Div:"))
+if users_choice == 1:
+    print (num_1 + num_2)'''
+
+user_choice = int(input ("pls enter any number: "))
+if user_choice % 2 == 0:
+    print(f"{user_choice} is an even number")
+else:
+    print(f"{user_choice} is odd number") 
+
+    
+'''
+code Q
+'''
