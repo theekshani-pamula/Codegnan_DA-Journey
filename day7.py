fd141b2243495c74e602f80779c267919918176d
Theekshani1103 <theekshani1104@gmail.com>
2026-03-27 20:54:44 +0530

    Tuple

--- day7.py
+++ day7.py
@@ -1,5 +1,47 @@
-a=int(input("enter the tuple value:"))
+'''a=int(input("enter the tuple value:"))
 b=int(input("enter the tuple value:"))
 c=int(input("enter the tuple value:"))
 t=(a,b,c)
-print(t)
+print(t)'''
+
+
+'''
+print(9+6)
+print("Python" + "Language")
+print([2+3] + [4+5])
+
+concatenation
+-------------
+this is nothing but, a (+) behavi..
+case-1:-
+intergers---this will act as addtion for the int
+
+case-2:-
+for other datatypes (we have to use same type) this (+) act as concatention
+if we try to access 2 different types, it can only concatent , only string to string  
+
+print(9+6)
+print("Theekshani" + "pamula")
+print([2+3] + [4+5])
+      
+tuple()---> it is collection of different data types and this is represented by () and then seprated by (,)
+eg...
+thing = (11,"Theeku",[18,23],(4,6))
+
+thing = (11,"Theeku",[18,23],(4,6))
+print(thing)
+
+thing = (12,89,"python",(23,"janu",[67,"python is a language",(7,8)],[8,("python"),[34,9]]))
+print(thing[3][2][1][9])
+
+num = 11
+num_2 = 19
+print(f"before swapping num = {num} and num_2 = {num_2}")
+num, num_2 = num_2, num
+print(f"after swapping num = {num} and num_2 = {num_2}")
+'''
+'''leap_year = int(input("enter year:"))
+if (leap_year % 4 == 0 and leap_year % 100 != 0) or leap_year % 400 == 0:
+   print(f"yes, {leap_year} is a leap year")
+else:
+     print(f"no, {leap_year} is not a leap year")'''
